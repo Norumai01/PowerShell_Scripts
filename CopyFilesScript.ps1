dffe3efaa7ff73bb90ab473ffc396c39ca9bac5d
Norumai01 <johnnydinhnguyen2001@gmail.com>
2024-05-25 19:38:43 -0400

    Updated file to copy and paste properly.

--- CopyFilesScript.ps1
+++ CopyFilesScript.ps1
@@ -12,18 +12,16 @@ function Copy-Files {
 	foreach ($SrcFile in $SrcFiles) {
 		# Check if source folder exists
 		CheckSourceFile -SrcFile $SrcFile
+		# Resolve full path for the source path, if necessary
+		$fullSrcPath = Resolve-Path -Path $SrcFile
 		
 		foreach ($DestFolder in $DestFolders) {
+			$fullDestFolder = Resolve-Path -Path $DestFolder
 			# Check if desination folder exists	
-			CheckDestFolder -DestFolder $DestFolder
-
-			# Join the destination folder with the child file
-			$destFilePath = Join-Path -Path $DestFolder -ChildPath (Get-Item $SrcFile).Name
+			CheckDestFolder -DestFolder $fullDestFolder
 
 			# Copy and paste
-            Copy-Item -Path $SrcFile -Destination $destFilePath -Force
-            Write-Host "Copied $SrcFile to $destFilePath"
-
+			CopyPaste -SrcFile $fullSrcPath -DestFolder $fullDestFolder
 		}
 	}
 }
@@ -45,6 +43,28 @@ function CheckDestFolder {
 	if (-not (Test-Path -Path $DestFolder)) {
 		New-Item -ItemType Directory -Path $DestFolder -Force
 		Write-Host "Created destination folder: $DestFolder"
+	}
+}
+function CopyPaste {
+	param (
+		[string]$SrcFile,
+		[string]$DestFolder
+	)
+	# Copy and paste file either a file or files in folder.
+	if (Test-Path -Path $SrcFile -PathType Container) {
+		# Source is a folder.
+		$DestFolderPath = Join-Path -Path $DestFolder -ChildPath (Get-Item $SrcFile).Name
+		if (-not (Test-Path -Path $DestFolderPath)) {
+			New-Item -ItemType Directory -Path $DestFolderPath -Force
+		}
+		Copy-Item -Path $SrcFile\* -Destination $DestFolderPath -Recurse -Force
+		Write-Host "Copied contents of folder $SrcFile to $DestFolderPath"
+	}
+	else {
+		# Source is a file.
+		$destFilePath = Join-Path -Path $DestFolder -ChildPath (Get-Item $SrcFile).Name
+		Copy-Item -Path $SrcFile -Destination $destFilePath -Force
+		Write-Host "Copied file $SrcFile to $destFilePath"
 	}
 }
 
